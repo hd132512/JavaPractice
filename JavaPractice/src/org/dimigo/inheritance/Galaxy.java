ad51654f95591c0cf706e9f7006f14585fe7b021
Jong <Jong@Jong-PC>
2015-08-25 10:38:03 +0900

    실습과제 2

---
+++ JavaPractice/src/org/dimigo/inheritance/Galaxy.java
@@ -0,0 +1,38 @@
+/**
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *   _ Galaxy
+ * 
+ * About :
+ * Date : 2015. 8. 25.
+ * </pre>
+ *
+ * @author	: 김종현
+ * @version : 1.0
+ */
+public class Galaxy extends SmartPhone {
+
+	
+	public Galaxy() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Galaxy(String model, String company, int price) {
+		super(model, company, price);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void pay(){
+		System.out.println("삼성 페이로 결제합니다.");
+	}
+	
+	public void useWirelessCharging(){
+		System.out.println("무선 충전 기능을 이용합니다.");
+	}
+
+}
---
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
@@ -0,0 +1,38 @@
+/**
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *   _ SmartPhoneTest
+ * 
+ * About :
+ * Date : 2015. 8. 25.
+ * </pre>
+ *
+ * @author	: 김종현
+ * @version : 1.0
+ */
+public class SmartPhoneTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SmartPhone phones[] = {
+				new IPhone("iPhone 6", "애플", 700000), 
+				new Galaxy("갤럭시 S6", "삼성",650000)};
+		
+		for(SmartPhone x : phones){
+			System.out.println(x);
+			x.turnOn();
+			x.pay();
+			x.useSpecialFunction();
+			x.turnOff();
+			System.out.println();
+		}
+
+	}
+
+}
